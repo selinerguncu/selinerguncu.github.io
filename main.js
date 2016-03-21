2e62fe9ebcd76d1cb2d052b44939dd954bbe7878
Selin Erguncu <erguncu@usc.edu>
2016-03-20 17:30:32 -0700

    added some fun

---
+++ main.js
@@ -0,0 +1,57 @@
+(function(){
+  var IMAGES     = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  // var resetTimer = null;
+  var safeToGo   = true;
+
+  var reset = function(){
+    var kk = document.getElementById('kk');
+    return setImage(IMAGES[0]);
+  };
+
+  var setImage = function(nextNumber){
+    var kk = document.getElementById('kk');
+    return kk.setAttribute('src', './img/'+nextNumber+'.png');
+  };
+
+  var goToNextImage = function(){
+    var kk         = document.getElementById('kk');
+    var src        = kk.getAttribute('src');
+    var kkNumber   = parseInt(src.replace('./img/','').replace('.png',''), 10);
+    var nextNumber = IMAGES[0];
+
+    for (imageNumber of IMAGES) {
+      if (imageNumber > kkNumber) {
+        nextNumber = imageNumber;
+        break
+      }
+    }
+    // if (resetTimer) {
+    //   clearTimeout(resetTimer);
+    // }
+    // resetTimer = setTimeout(reset, 2000);
+    return setImage(nextNumber);
+  };
+
+  var cacheImages = function(){
+    for (imageNumber of IMAGES) {
+      img = new Image
+      img.src = './img/'+imageNumber+'.png'
+    }
+  };
+
+  document.addEventListener('mousemove', function(event){
+
+    if (event.target.tagName == 'IMG') return;
+
+    if (safeToGo) {
+      safeToGo = false;
+      setTimeout(function(){
+        goToNextImage()
+        safeToGo = true;
+      }, 200);
+    }
+
+  });
+
+  cacheImages();
+})();
